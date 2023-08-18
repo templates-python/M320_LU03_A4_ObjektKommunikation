510625c0044ae8ef84d117e76d1bbd14b9a46967
Rene Probst <rene.probst@bzz.ch>
2023-08-18 15:36:12 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+from instrument import Instrument
+from musician import Musician
 
 
-if __name__ == '__main__':
-    calculate()
+#   main.py dien t der Ausführung der Applikation.
+#   Es werden die benötigen Objekte erzeugt und die
+#   Methoden gemäss Vorgabe aufgerufen.
+
+if __name__ == "__main__":
+    # Erzeugen der benötigten Objekte für die Band
+    # TODO
+    guitar = Instrument('Gitarre')
+    piano = Instrument('Klavier')
+    max = Musician('Max', guitar)
+    moritz = Musician('Moritz', piano)
+    # Den Ablauf gemäss Sequenzdiagramm umsetzen
+    # TODO
+    max.play_tone('a')
+    moritz.play_tone('f')
