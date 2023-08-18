ca6a7579530c6b6359fcddc22833252fce45b445
Rene Probst <rene.probst@bzz.ch>
2023-08-18 15:39:52 +0200

    main adjusted

--- main.py
+++ main.py
@@ -9,11 +9,7 @@ from musician import Musician
 if __name__ == "__main__":
     # Erzeugen der benötigten Objekte für die Band
     # TODO
-    guitar = Instrument('Gitarre')
-    piano = Instrument('Klavier')
-    max = Musician('Max', guitar)
-    moritz = Musician('Moritz', piano)
+
     # Den Ablauf gemäss Sequenzdiagramm umsetzen
     # TODO
-    max.play_tone('a')
-    moritz.play_tone('f')
+
