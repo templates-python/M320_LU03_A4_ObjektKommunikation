c8830c949b3c540df13b05bf6e37c6cfca947551
Marcel Suter <marcel.suter@bzz.ch>
2023-08-25 07:58:36 +0200

    added tests, coding standards applied

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from musician import Musician
 #   Es werden die benötigen Objekte erzeugt und die
 #   Methoden gemäss Vorgabe aufgerufen.
 
-if __name__ == "__main__":
-    # Erzeugen der benötigten Objekte für die Band
-    # TODO
+def main():
     guitar = Instrument('Gitarre')
     piano = Instrument('Klavier')
     max = Musician('Max', guitar)
     moritz = Musician('Moritz', piano)
-    # Den Ablauf gemäss Sequenzdiagramm umsetzen
-    # TODO
+
     max.play_tone('a')
     moritz.play_tone('f')
+
+if __name__ == "__main__":
+    main()
--- musician.py
+++ musician.py
@@ -1,9 +1,9 @@
 import instrument
 
 class Musician:
-    '''
+    """
     Beschreibt einen Musiker, der ein Instrument spielt und auf diesem Töne erzeugt.
-    '''
+    """
 
     def __init__(self, name, instrument):
         '''
@@ -16,15 +16,15 @@ class Musician:
 
     @property
     def name(self):
-        '''
+        """
         Liefert den Namen des Musikers
         :return: Name des Musikers
-        '''
+        """
         return self._name
 
     def play_tone(self, the_tone):
-        '''
+        """
         Spielt den angegeben Ton
         :param the_tone: der gespielte Ton
-        '''
+        """
         print(f'{self._name} spielt mit {self._instrument.play_tone(the_tone)}')
